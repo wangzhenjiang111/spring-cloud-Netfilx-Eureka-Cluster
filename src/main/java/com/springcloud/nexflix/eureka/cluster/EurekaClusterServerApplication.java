dde92bd2b14901b97e1ecb3c5951b853fa116962
wangzhenjiang111 <wangzhenjiang@iauto.com>
2021-03-06 18:36:23 +0800

    init

---
+++ src/main/java/com/springcloud/nexflix/eureka/cluster/EurekaClusterServerApplication.java
@@ -0,0 +1,14 @@
+package com.springcloud.nexflix.eureka.cluster;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaClusterServerApplication {
+
+	public static void main(String[] args) {		
+		SpringApplication.run(EurekaClusterServerApplication.class, args);
+	}
+
+}
